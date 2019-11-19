2149157576c28c54e7715ee238aef847d200bf96
Nejc <nejc.stibilj01@gmail.com>
2019-11-19 13:01:32 +0100

    Naloga 1 resena preko jDoodle

--- HomeWork/hw/Naloga1.java
+++ HomeWork/hw/Naloga1.java
@@ -2,9 +2,20 @@
 public class Naloga1 {
 
 	public static void main(String[] args) {
-
-	//alo
 		
+/*
+ * 	Naloga 1
+ * 	Napiši program, ki bo izračunal in izpisal rezultata račinov: 
+ * 	1.) (10 + 5) / 3 = ?
+ * 	2.) (22+99) * (113/14) 
+ */	
+		
+		int prviRezultat = (10+5) / 3;		
+		int drugiRezultat = (22+99) * (113/14);		
+		
+		System.out.println("Rezultat prve naloge je: "+prviRezultat+"\nRezultat druge naloge je: "+drugiRezultat);
+
+	
 	}
 
 }
