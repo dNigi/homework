063d347cfabe928f42078b17fea932d33a6b47a5
HNNX <nejc.stibilj01@gmail.com>
2019-11-22 19:29:38 +0100

    Naloga2 finished

--- HomeWork/hw/Naloga2.java
+++ HomeWork/hw/Naloga2.java
@@ -2,40 +2,21 @@ import java.util.Arrays;
 
 public class Naloga2 {
 
-	public static void main(String[] args) {
-		
-/*
-Naloga 2
-Program naj sprejme 5 argumentov v obliki celega števila. 
-Program naj izpiše najmanjše število, največje število in 
-povprečno vrednost vseh števil.
-*/
-		
-		
-		/*
+	public static void main(String[] args) {		
+
 		int prvoStevilo = Integer.parseInt(args[0]);
 		int drugoStevilo = Integer.parseInt(args[1]);
 		int tretjeStevilo = Integer.parseInt(args[2]);
 		int cetrtoStevilo = Integer.parseInt(args[3]);
 		int petoStevilo = Integer.parseInt(args[4]);
-		*/
-		int x,y,z,k,j;
-		
-		x = 66;
-		y = 22;
-		z = 80;
-		k = 23;
-		j = 90;
-		
-		
-		//int[] vsaStevila = {prvoStevilo, drugoStevilo, tretjeStevilo, cetrtoStevilo, petoStevilo};
-		int[] vsaStevila = {x,y,z,k,j};
-		
+
+				
+		int[] vsaStevila = {prvoStevilo, drugoStevilo, tretjeStevilo, cetrtoStevilo, petoStevilo};
 		
 		Arrays.sort(vsaStevila);	
 		
 		int najmanjseStevilo = vsaStevila[0];
-		int najvecjeStevilo = vsaStevila.length-1;
+		int najvecjeStevilo = vsaStevila[ vsaStevila.length-1 ];
 		
 		int temp = 0;
 		
@@ -43,9 +24,12 @@ povprečno vrednost vseh števil.
 		temp += i;			
 		}
 		
-		System.out.println("ZDEJ VN + "+temp);
-		
-		System.out.println(temp / vsaStevila.length);
+		int average = temp/vsaStevila.length;
+	
+
+		System.out.println("Najmanjse vneseno stevilo: "+najmanjseStevilo);
+		System.out.println("Najvecje vneseno stevilo: "+najvecjeStevilo);
+		System.out.println("Povprecje vnesenih stevil: "+average);
 		
 		
 		
