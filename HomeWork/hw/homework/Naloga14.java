30c204c229bd149c7d5900319dc6743330ea2034
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 21:42:01 +0100

    Naloga 14 resena

--- HomeWork/hw/homework/Naloga14.java
+++ HomeWork/hw/homework/Naloga14.java
@@ -3,38 +3,45 @@ package homework;
 public class Naloga14 {
 
 	public static void main(String[] args) {
-		
-/*
-Program naj izpise tabelo
-Program naj izpise vsa stevila v tabeli, ki so deljiva s 7
-Program naj sesteje vsa stevila v tabeli in izpse rezultat
 
- */
-		
-		
-		
+
+
 		int[][] twoDimensional = new int[14][9];
-		
-		
+
+		//Izpis Tabele ::>
+		System.out.println("Izpis tabele:\n");
 		for (int i = 0; i < twoDimensional.length; i++) {
 			for (int j = 0; j < twoDimensional[i].length; j++) {
-				
 				twoDimensional[i][j] = (int) (Math.random()*70+1);
-				
-				//Izpis Tabele ::>
-				//System.out.print("  "+twoDimensional[i][j]+" ");			
-				//Izpis tabele / 7 ::>
-				
+				System.out.print(twoDimensional[i][j]+" ");
+			}
+			System.out.println("");
+		}
+
+		System.out.println("");
+		//Izpis tabele / 7 ::>
+		System.out.println("Izpis stevil deljivih s 7:\n");
+		for(int i = 0; i < twoDimensional.length; i++) {
+			for(int j = 0; j < twoDimensional[i].length; j++) {
 				if(twoDimensional[i][j] % 7 == 0){
 					System.out.println(twoDimensional[i][j] +" je deljivo s 7");
 				}
-
 			}
-			
-			
 		}
 
-	
+		int temp = 0;
+
+		System.out.println("");
+		//Sestevek tabele ::>
+		System.out.println("Sestevek tabele:\n");
+
+		for(int i = 0; i < twoDimensional.length;i++) {
+			for(int j = 0; j < twoDimensional[i].length;j++) {
+				temp += twoDimensional[i][j];			}
+		}
+		System.out.println(">> "+ temp);
+
+
 	}
 
 }
