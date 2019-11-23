3c7bb0fda8bd0e5764cbea7421b7645146ce34fc
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 09:19:52 +0100

    Naloga4 done

---
+++ HomeWork/hw/Naloga4.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+
+public class Naloga4 {
+
+	public static void main(String[] args) {
+		
+		Calendar cal = Calendar.getInstance();		
+		int currentYear = cal.get(Calendar.YEAR);
+		
+		int letnicaRojstva = Integer.parseInt(args[0]);		
+		int starost = currentYear - letnicaRojstva;
+		int manjkajocaLeta = 18 - starost;
+				
+		if(starost > 18)
+			System.out.println("Lahko se udelezite volitev.");
+		else
+			System.out.println("Zal ste premladi.\nVolitev se lahko udelezite cez "+manjkajocaLeta +" let.");
+		
+
+		
+
+
+	}
+
+}
