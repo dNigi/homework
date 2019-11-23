470531f9f5ea19c048e2aa7e0069ccdfab490a38
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 09:27:59 +0100

    Naloga 5 done

---
+++ HomeWork/hw/Naloga5.java
@@ -0,0 +1,19 @@
+public class Naloga5 {
+
+	public static void main(String[] args) {
+
+		String vneseniNiz = args[0];
+
+		char[] nizVArray = vneseniNiz.toCharArray();		
+
+		System.out.println("Vneseni niz: " + vneseniNiz);
+
+		for (int i = 0; i < nizVArray.length; i = i +2) {
+
+			System.out.print(nizVArray[i]);
+
+		}
+
+	}
+
+}
