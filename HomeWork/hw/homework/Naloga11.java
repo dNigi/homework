388c0302a58472e6bd5aeb02398da83d7facafba
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 18:15:12 +0100

    Naloga 11 done

---
+++ HomeWork/hw/homework/Naloga11.java
@@ -0,0 +1,53 @@
+package homework;
+
+public class Naloga11 {
+
+	public static void main(String[] args) {
+		
+		int mesec = 1;
+		
+		switch (mesec) {
+		case 1:
+			System.out.println("Januar");
+			break;
+		case 2:
+			System.out.println("Februar");
+			break;
+		case 3:
+			System.out.println("Marec");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("Maj");
+			break;
+		case 6:
+			System.out.println("Junij");
+			break;
+		case 7:
+			System.out.println("Julij");
+			break;
+		case 8:
+			System.out.println("Avgust");
+			break;
+		case 9:
+			System.out.println("September");
+			break;
+		case 10:
+			System.out.println("Oktober");
+			break;
+		case 11:
+			System.out.println("November");
+			break;
+		case 12:
+			System.out.println("December");
+			break;
+		default:
+			System.out.println("\"Great Scott!\"");
+			break;
+		}
+
+	}
+
+}
