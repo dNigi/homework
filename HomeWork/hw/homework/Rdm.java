792ee0b5630997c4a716f5ac97c7263f00c178b0
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 12:02:25 +0100

    jhwlka

---
+++ HomeWork/hw/homework/Rdm.java
@@ -0,0 +1,18 @@
+package homework;
+
+public class Rdm {
+
+	public static void main(String[] args) {
+		
+		System.out.println("    *     ");
+		System.out.println("   ***    ");
+		System.out.println("  *****   ");
+		System.out.println(" *******  ");
+		System.out.println("   ***    ");
+		System.out.println("   ***    ");
+
+	
+	
+	}
+
+}
