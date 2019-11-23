7e68528a5851a33900deb741b01f65467942e8ad
HNNX <nejc.stibilj01@gmail.com>
2019-11-23 11:27:31 +0100

    test

--- HomeWork/hw/homework/Naloga9.java
+++ HomeWork/hw/homework/Naloga9.java
@@ -2,9 +2,22 @@ package homework;
 
 public class Naloga9 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)  {
+		
+		int steviloVrstic = 50;
+		
+		
+		for(int i = steviloVrstic; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+				
+			}
+			System.out.println("");
+			
+		}
 
+	
+	
 	}
 
 }
