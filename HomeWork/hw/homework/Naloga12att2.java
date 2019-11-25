62200461e6d2bcca8d1f2f67be8a447a5f6168b2
Nejc <nejc.stibilj01@gmail.com>
2019-11-25 20:46:45 +0100

    ye

---
+++ HomeWork/hw/homework/Naloga12att2.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class Naloga12att2 {
+
+	public static void main(String[] args) {
+		
+		
+		String input = "123456";
+		
+		//Turn string to INT, calculate first 3 + last 3
+
+	
+	}
+
+}
