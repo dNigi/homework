be5cc5f7b1e14315d3081840fff9da851b32571e
Nejc <nejc.stibilj01@gmail.com>
2019-11-29 18:01:12 +0100

    ye

--- HomeWork/hw/homework/Naloga15.java
+++ HomeWork/hw/homework/Naloga15.java
@@ -8,10 +8,21 @@ public class Naloga15 {
  * V programu definiraj tabelo 4 dimenzionalno tabelo tipa int
  *  poljubne velikosti. Tabelo napolni z nakljucnimi vrednostmi
  *   med 1 in 10 ter tabelo izpisi.
+ *   
  */
 		
+		int tabela[][][][] = new int[10][10][10][10];
 		
-		int[][][][] fDim = new int[10][10][10][10];
+		
+		System.out.println("zacni polnit tabelo");
+		
+		
+		for (int i = 0; i < tabela.length; i++) {
+			
+		}
+		
+		
+
 		
 		
 	}
