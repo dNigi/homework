04d76b1730d2344d191a2137235b06c0adf0717f
Nejc <nejc.stibilj01@gmail.com>
2019-11-29 18:59:24 +0100

    Naloga 12 done!

--- HomeWork/hw/homework/Naloga12att2.java
+++ HomeWork/hw/homework/Naloga12att2.java
@@ -4,14 +4,25 @@ public class Naloga12att2 {
 
 	public static void main(String[] args) {
 		
-		
-		String input = "123456";
-		
-		//Turn string to INT, calculate first 3 + last 3
-		
-		Integer.parseInt(input);
-
+		String input = args[0];
+				
+		if(input.length() != 6) {
+			System.out.println("Stevilo ne ustreza pogojem");
+		}
+		else {			
+			System.out.println("Vneseno stevilo ("+ input+") razdelimo na dva dela \n"+ input.substring(0,3) +" in "+ input.substring(3,6));
+			System.out.println("Rezultat je:");
+			rez(input);			
+			
+		}	
+	}
 	
+	public static void rez(String vnos) {
+		
+		int prviDel = Integer.parseInt(vnos.substring(0,3));
+		int drugiDel = Integer.parseInt(vnos.substring(3,6));
+		System.out.print(prviDel+drugiDel);
+		
 	}
 
 }
