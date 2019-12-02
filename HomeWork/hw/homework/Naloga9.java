4cf9c3f2adc02776906c5429e800de57c49c4847
HNNX <nejc.stibilj01@gmail.com>
2019-12-02 19:42:37 +0100

    Popravljene naloge

--- HomeWork/hw/homework/Naloga9.java
+++ HomeWork/hw/homework/Naloga9.java
@@ -4,31 +4,25 @@ public class Naloga9 {
 
 	public static void main(String[] args)  {
 
-		//int steviloVrstic = Integer.parseInt(args[0]);
-		int steviloVrstic = 10;
+		int steviloVrstic = (int) (Math.random()*15+3);
 
-		if(steviloVrstic > 3 &&  steviloVrstic < 15) {
-
-			for(int i = steviloVrstic; i > 0; i--) {
-				for(int j = 0; j < i; j++) {
-					System.out.print("*");
-				}
-				
-				System.out.println("");
-
+		for(int i = steviloVrstic; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
 			}
-			
-			
-			for(int z = 0; z < steviloVrstic; z ++) {
-				for(int g = 0; g < z; g++) {
-					System.out.print("*");
-				}
-				System.out.println("");
-					
-			}
-			
+
+			System.out.println("");
 
 		}
+
+
+		for(int i = 0; i < steviloVrstic; i ++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+
+		}	
 
 	}
 
