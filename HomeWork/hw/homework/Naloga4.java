d31105f35a1524a2ff210b53fbb1a525fe84c46f
HNNX <nejc.stibilj01@gmail.com>
2019-12-02 18:41:01 +0100

    naloga4 popravek

--- HomeWork/hw/homework/Naloga4.java
+++ HomeWork/hw/homework/Naloga4.java
@@ -4,21 +4,39 @@ import java.util.Calendar;
 public class Naloga4 {
 
 	public static void main(String[] args) {
-		
+
 		Calendar cal = Calendar.getInstance();		
 		int currentYear = cal.get(Calendar.YEAR);
-		
-		int letnicaRojstva = Integer.parseInt(args[0]);		
+
+		int letnicaRojstva = 0;
+		boolean preveriLetnico = true;
+
+		while(preveriLetnico) {
+			if(letnicaRojstva > 1900 || letnicaRojstva < currentYear + 1) {
+				System.out.println("vnesite veljavno letnico");
+				System.exit(0);				
+			}
+
+			else {
+				letnicaRojstva = Integer.parseInt(args[0]);
+				break;
+
+			}
+		}
+
+
+
+
 		int starost = currentYear - letnicaRojstva;
 		int manjkajocaLeta = 18 - starost;
-				
+
 		if(starost > 18)
 			System.out.println("Lahko se udelezite volitev.");
 		else
 			System.out.println("Zal ste premladi.\nVolitev se lahko udelezite cez "+manjkajocaLeta +" let.");
-		
 
-		
+
+
 
 
 	}
