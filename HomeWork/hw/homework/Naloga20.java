cf662445432883e69d0f4c99ae791d3bfb91cc4e
HNNX <nejc.stibilj01@gmail.com>
2019-12-06 16:33:44 +0100

    naloga 20 attempt

---
+++ HomeWork/hw/homework/Naloga20.java
@@ -0,0 +1,26 @@
+package homework;
+
+public class Naloga20 {
+
+	public static void main(String[] args) {
+
+	
+	}
+	
+	public static double tabAverage(int[] arr) {
+		
+		int vsaStevila = 0;
+		double average = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			vsaStevila += arr[i];
+			System.out.println(vsaStevila);
+						
+		}
+		
+		return average;
+		
+	}
+
+}
