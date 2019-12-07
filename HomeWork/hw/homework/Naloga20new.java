2c4ed88fe9de665387e17efff9bda9b57c2dfc02
HNNX <nejc.stibilj01@gmail.com>
2019-12-07 09:25:06 +0100

    Naloga 20 done

---
+++ HomeWork/hw/homework/Naloga20new.java
@@ -0,0 +1,45 @@
+package homework;
+
+public class Naloga20new {
+
+	public static void main(String[] args) {
+		
+		int[] tabela = new int[50];
+		
+		polnjeneTabele(tabela, 1000, 5000);
+		
+		izpisTabele(tabela);
+		System.out.println("\n");
+		System.out.println("Povprecna vrednost: " + povprecnaVrednostStevil(tabela));
+		
+	}
+	
+	
+	public static double povprecnaVrednostStevil(int[] arr) {		
+		int sestevekStevil = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			sestevekStevil += arr[i];
+		}
+		
+		return (double) sestevekStevil / arr.length;
+	}
+	
+	
+	public static void izpisTabele(int[] arr) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void polnjeneTabele(int[] arr, int min, int max) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			arr[i] = (int) (Math.random()* max + min);
+		}
+		
+	}
+
+}
