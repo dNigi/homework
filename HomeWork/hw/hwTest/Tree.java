a3f37fe1b070ca177cdab479f3a508eb1dda01f2
Nejc <nejc.stibilj01@gmail.com>
2019-12-08 19:30:52 +0100

    kroshnja

--- HomeWork/hw/hwTest/Tree.java
+++ HomeWork/hw/hwTest/Tree.java
@@ -1,21 +1,44 @@
 package hwTest;
 
+import java.util.Scanner;
+
 public class Tree {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
+		Scanner sc = new Scanner(System.in);
 		
-		int rows = 5;
+		System.out.println("Visina trikoitnika?");
+		int rows = sc.nextInt() - 1;
+				
 		
-		for (int i = 0; i < rows; i++) {
+		for (int i = 0; i <= rows; i++) {
+			
 			
 			for (int j = rows; j > i; j--) {
-				System.out.print(j);
+				System.out.print(" ");
 			}
 			
-			System.out.println("x");
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			
+			Thread.sleep(220);
+			
+			System.out.println("");
 			
 		}
+		sc.close();
+		
+		System.out.println("Za");
+		Thread.sleep(320);
+		System.out.println("Misev");
+		Thread.sleep(320);
+		System.out.println("BIRZDEJ ki ima "+ rows + "let!!");
 
 	
 	}
