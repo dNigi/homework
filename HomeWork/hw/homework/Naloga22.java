93c179ab80dff5e2c37b6202e3fc1090826d334f
HNNX <nejc.stibilj01@gmail.com>
2019-12-11 19:49:54 +0100

    ff

--- HomeWork/hw/homework/Naloga22.java
+++ HomeWork/hw/homework/Naloga22.java
@@ -8,7 +8,7 @@ public class Naloga22 {
 //Napisi program, ki bo uporabnika vprasal naj vnese poljuben niz. 
 //Vneseni niz naj nato zapise v datoteko mojiNizi.txt. 
 //Program naj ohrani prej zabelezene nize.
-
+ 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
