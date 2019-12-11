2046d0b2c33e6316dbdad7a0621b09e501343bfb
HNNX <nejc.stibilj01@gmail.com>
2019-12-11 19:37:08 +0100

    Naloga22 start

---
+++ HomeWork/hw/homework/Naloga22.java
@@ -0,0 +1,27 @@
+package homework;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Naloga22 {
+	
+//Napisi program, ki bo uporabnika vprasal naj vnese poljuben niz. 
+//Vneseni niz naj nato zapise v datoteko mojiNizi.txt. 
+//Program naj ohrani prej zabelezene nize.
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String fileName = "";
+		
+		String vnos = sc.nextLine();
+		
+		File dat = new File(vnos);
+		
+		
+
+	
+	}
+
+}
