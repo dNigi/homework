d5b78f8a6d48c9e441792e196493f0b4eab709f1
Nejc <nejc.stibilj01@gmail.com>
2019-12-12 16:05:14 +0100

    naloga 23 in progress

--- HomeWork/hw/homework/Naloga23.java
+++ HomeWork/hw/homework/Naloga23.java
@@ -1,10 +1,42 @@
 package homework;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Naloga23 {
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//Program naj iz prilozene datoteke prebere stevila ter jih sesteje.
+//Rezultat naj se sharni v datoteko, pod zadnjim stevilom.
 
+	public static void main(String[] args) throws IOException {	
+		
+		File datoteka = new File("stevilke.txt");	
+		
+		
+		System.out.println("Beri datoteke boilerplate: ");		
+		
+		try {
+			BufferedReader beriDatoteko = new BufferedReader( new FileReader(datoteka));
+			
+			String line;
+			
+			while(  (line = beriDatoteko.readLine()) != null ) {
+				System.out.println(line);
+			}
+			
+			beriDatoteko.close();
+		} catch (IOException e) {
+			System.out.println("Napaka");
+		}
+		
+		
+		
+		
+
+	
 	}
 
 }
